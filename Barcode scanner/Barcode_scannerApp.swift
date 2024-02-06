50dd5dc623c20d7b3fe6d4b2c2b3ecdafd6adf33
Gianmarco <gimmy03@icloud.com>
2024-02-06 22:33:10 +0100

    Initial Commit

---
+++ Barcode scanner/Barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_scannerApp.swift
+//  Barcode scanner
+//
+//  Created by Gianmarco on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
